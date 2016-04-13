9a25fa0f7be7b0c61353dd3900b5b8d7e532c2fb
andyafter <andyafter@gmail.com>
2016-04-13 15:15:02 +0800

    Added init-theme.el for myself.

---
+++ personal/init-theme.el
@@ -0,0 +1,9 @@
+;;; Code:
+;;; checkout what is the best practice later.
+(prelude-require-packages '(sublime-themes base16-theme))
+(load-theme 'fogus t)
+
+(set-frame-parameter (selected-frame) 'alpha '(90 90))
+;(add-to-list 'default-frame-alist '(alpha 85 85))
+
+(provide 'init-theme)
