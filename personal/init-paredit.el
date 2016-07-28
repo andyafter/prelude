ff5f0f5de940e388eb4768068fc0503e2ab58ae8
VR_room <VR_room>
2016-07-28 13:49:26 +0800

    Added smartparens

--- personal/init-paredit.el
+++ personal/init-paredit.el
@@ -1,8 +1,12 @@
 ;;; paredit everywhere
 ;;; Code:
 (prelude-require-package 'paredit-everywhere)
+(prelude-require-package 'smartparens)
+
 (require 'paredit-everywhere)
+(require 'smartparens)
 (add-hook 'prog-mode-hook 'paredit-everywhere-mode)
+
 
 (provide 'init-paredit)
 ;;; end for initiation of paredit
