391a6b0c2ed149021a29dc693b4c0d604e0a0174
andyafter <andyafter@gmail.com>
2017-10-12 17:48:28 +0800

    Corrected go hooks

--- personal/init-go.el
+++ personal/init-go.el
@@ -8,15 +8,6 @@
 
 (defun my-go-mode-hook ()
     ; Call Gofmt before saving 
-    (add-hook 'before-save-hook 'gofmt-before-save))
-
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (replace-regexp-in-string
-                          "[ \t\n]*$"
-                          ""
-                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq eshell-path-env path-from-shell) ; for eshell users
-    (setq exec-path (split-string path-from-shell path-separator))))
+  (add-hook 'before-save-hook 'gofmt-before-save))
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
